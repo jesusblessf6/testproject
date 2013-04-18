584bd6fbca0517db06b0779501056b2043c73f37
jesusblessf6 <jesusblessf6@gmail.com>
2013-04-19 00:02:33 +0800

    add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Testproject::Application.routes.draw do
+  get "static_pages/about"
+
+  get "static_pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
