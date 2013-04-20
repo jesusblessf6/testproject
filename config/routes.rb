d88d4a508a7c98315c44af0550ada61ac3052e35
jesusblessf6 <jesusblessf6@gmail.com>
2013-04-20 17:55:11 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Testproject::Application.routes.draw do
-  get "static_pages/about"
+  #get "static_pages/about"
+  match 'about' => 'static_pages#about'
 
   get "static_pages/home"
+
+  get "static_pages/help"
+  match 'help' => 'static_pages#help'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +57,7 @@ Testproject::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
